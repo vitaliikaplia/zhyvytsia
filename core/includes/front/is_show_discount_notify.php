177e5935b08eb3efc15c48049a31b0447c06a50c
vitaliikaplia <vitalii.kaplia@gmail.com>
2025-09-25 22:34:17 +0300

    remove discount logic

--- core/includes/front/is_show_discount_notify.php
+++ core/includes/front/is_show_discount_notify.php
@@ -2,16 +2,16 @@
 
 if(!defined('ABSPATH')){exit;}
 
-function is_show_discount_notify(){
-
-    $ids_arr = array_filter(explode(".", wp_unslash(stripslashes($_COOKIE['cart']))));
-    $filtered_ids = array_filter($ids_arr, function($page_id) {
-        $allow_discount = get_field('allow_discount', $page_id);
-        return $allow_discount === true;
-    });
-    $ids_arr_count = count($filtered_ids);
-    // $difference = intval(get_option('options_shop_minimum_quantity_of_products_in_the_cart_for_wholesale_discount')) - $ids_arr_count;
-
-    return $ids_arr_count < intval(get_option('options_shop_minimum_quantity_of_products_in_the_cart_for_wholesale_discount'));
-
-}
+//function is_show_discount_notify(){
+//
+//    $ids_arr = array_filter(explode(".", wp_unslash(stripslashes($_COOKIE['cart']))));
+//    $filtered_ids = array_filter($ids_arr, function($page_id) {
+//        $allow_discount = get_field('allow_discount', $page_id);
+//        return $allow_discount === true;
+//    });
+//    $ids_arr_count = count($filtered_ids);
+//    // $difference = intval(get_option('options_shop_minimum_quantity_of_products_in_the_cart_for_wholesale_discount')) - $ids_arr_count;
+//
+//    return $ids_arr_count < intval(get_option('options_shop_minimum_quantity_of_products_in_the_cart_for_wholesale_discount'));
+//
+//}
